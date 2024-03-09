30f04b9ae76dd985b072deb6dcfcc028ed57dbca
tinyhodler <85483152+tinyhodler@users.noreply.github.com>
2024-03-09 11:06:53 +0800

    Update main.py
    edited the code to generate infinite wallet addresses

--- main.py
+++ main.py
@@ -8,20 +8,21 @@ class BlockChainAccount():
         self.coin_type = coin_type
         self.password = password # if have password
 
-    def get_address_pk(self):
+    def get_address_pk(self,accountNo):
 
         seed_bytes = Bip39SeedGenerator(self.mnemonic).Generate(self.password)
         if self.coin_type != Bip44Coins.SOLANA:
-            bip44_mst_ctx = Bip44.FromSeed(seed_bytes, self.coin_type).DeriveDefaultPath()
+            # bip44_mst_ctx = Bip44.FromSeed(seed_bytes, self.coin_type).DeriveDefaultPath()
+            bip44_mst_ctx = Bip44.FromSeed(seed_bytes, self.coin_type)
+            bip44_mst_ctx.Purpose().Coin().Account(accountNo)
             return bip44_mst_ctx.PublicKey().ToAddress(), bip44_mst_ctx.PrivateKey().Raw().ToHex()
         else:
             bip44_mst_ctx = Bip44.FromSeed(seed_bytes, self.coin_type)
            
-            bip44_acc_ctx = bip44_mst_ctx.Purpose().Coin().Account(0)
+            bip44_acc_ctx = bip44_mst_ctx.Purpose().Coin().Account(accountNo)
             bip44_chg_ctx = bip44_acc_ctx.Change(Bip44Changes.CHAIN_EXT) # if you use "Solflare", remove this line and make a simple code modify and test
             priv_key_bytes = bip44_chg_ctx.PrivateKey().Raw().ToBytes()
             public_key_bytes = bip44_chg_ctx.PublicKey().RawCompressed().ToBytes()[1:]
             key_pair = priv_key_bytes+public_key_bytes
 
             return bip44_chg_ctx.PublicKey().ToAddress(), base58.Base58Encoder.Encode(key_pair)
-
